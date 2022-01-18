dcbbb511f88c452df284441c73710d3f543cc00f
depalma99 <85015921+depalma99@users.noreply.github.com>
2022-01-18 19:59:57 +0100

    Update AutenticazioneService.java

--- src/main/java/PartnerShop/autenticazione/service/AutenticazioneService.java
+++ src/main/java/PartnerShop/autenticazione/service/AutenticazioneService.java
@@ -1,7 +1,12 @@
 package PartnerShop.autenticazione.service;
 
 import PartnerShop.model.entity.Cliente;
+import PartnerShop.model.entity.UtenteRegistrato;
+
+import java.util.List;
 
 public interface AutenticazioneService {
-    Cliente findClienteByEmail(String email);
+
+    UtenteRegistrato login(String email, String password);
+    Cliente findClienteByUsernamePass(String username,String password);
 }
