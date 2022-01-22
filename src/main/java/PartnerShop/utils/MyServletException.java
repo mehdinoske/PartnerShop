590d020ad71c4fb823ef34edce286b55975641d1
depalma99 <85015921+depalma99@users.noreply.github.com>
2022-01-22 20:55:07 +0100

    update gestioneprodotto

---
+++ src/main/java/PartnerShop/utils/MyServletException.java
@@ -0,0 +1,26 @@
+package PartnerShop.utils;
+
+import PartnerShop.model.entity.UtenteRegistrato;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+public class MyServletException extends ServletException  {
+
+    public MyServletException() {
+        super();
+    }
+
+    public MyServletException(String message, Throwable rootCause) {
+        super(message, rootCause);
+    }
+
+    public MyServletException(String message) {
+        super(message);
+    }
+
+    public MyServletException(Throwable rootCause) {
+        super(rootCause);
+    }
+
+}
