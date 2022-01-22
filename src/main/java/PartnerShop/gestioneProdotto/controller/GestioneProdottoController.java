37cfd2fc8ead1805676abfbb6e5e58cbe81ee934
depalma99 <85015921+depalma99@users.noreply.github.com>
2022-01-22 21:51:50 +0100

    Update GestioneProdottoController.java

--- src/main/java/PartnerShop/gestioneProdotto/controller/GestioneProdottoController.java
+++ src/main/java/PartnerShop/gestioneProdotto/controller/GestioneProdottoController.java
@@ -29,34 +29,78 @@ public class GestioneProdottoController extends HttpServlet {
         Prodotto prodotto;
         UtenteRegistrato ut;
         Amministratore adm;
-        int id;
-        System.out.println(s);
+        String nome, descrizione, categoria;
+        long prezzo_Cent;
+        int disponibilita, id;
+
         switch (s) {
 
             case "/prodotto-visualizza":
-                id = Integer.parseInt(request.getParameter("id"));
-                prodotto = PrDAO.getProdottoById(id);
-                (prodotto == null) {
-                throw new MyServletException("Prodotto non trovato.");
+                try {
+                    id = Integer.parseInt(request.getParameter("id"));
+                } catch (NumberFormatException e) {
+                    throw new MyServletException("Id prodotto non corretto.");
                 }
-                if(prodotto != null) {
+                prodotto = PrDAO.getProdottoById(id);
+                if(prodotto == null) {
+                throw new MyServletException("Prodotto non trovato.");
+                } else {
                     request.setAttribute("prodotto", prodotto);
                     requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/prodotto.jsp");
-                    //System.out.println("visualizzato");
-                } else {
-                    request.setAttribute("messaggio", "Nessun prodotto trovato.");
-                    requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/notifica.jsp");
-                    //System.out.println("non trovato");
+                    requestDispatcher.forward(request, response);
                 }
-                requestDispatcher.forward(request, response);
                 break;
 
             case "/prodotto-modifica-form":
-                id = Integer.parseInt(request.getParameter("id"));
-                prodotto = PrDAO.getProdottoById(id);
-                if(prodotto != null) {
-                    request.setAttribute("prodotto", prodotto);
-                    requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/modificaProdotto.jsp");
+                ut = (UtenteRegistrato) request.getSession().getAttribute("utente");
+                if(ut == null || ut.getTipo() == 0) {
+                    throw new MyServletException("Non hai i permessi necessari.");
+                }   else {
+                    try {
+                        id = Integer.parseInt(request.getParameter("id"));
+                    } catch (NumberFormatException e) {
+                        throw new MyServletException("Id prodotto non corretto.");
+                    }
+                    prodotto = PrDAO.getProdottoById(id);
+                    if(prodotto == null) {
+                        throw new MyServletException("Prodotto non trovato.");
+                    } else {
+                        request.setAttribute("prodotto", prodotto);
+                        requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/modificaProdotto.jsp");
+                        requestDispatcher.forward(request, response);
+                    }
+                }
+                break;
+
+            case "/prodotto-modifica":
+                ut = (UtenteRegistrato) request.getSession().getAttribute("utente");
+                if(ut == null || ut.getTipo() == 0) {
+                    throw new MyServletException("Non hai i permessi necessari.");
+                }   else {
+                    try {
+                        id = Integer.parseInt(request.getParameter("id"));
+                        nome = request.getParameter("nome");
+                        descrizione = request.getParameter("descrizione");
+                        categoria = request.getParameter("categoria");
+                        prezzo_Cent = Long.parseLong(request.getParameter("prezzo_Cent"));
+                        disponibilita = Integer.parseInt(request.getParameter("disponibilita"));
+                    } catch (Exception e) {
+                        throw new MyServletException("Dati non corretti.");
+                    }
+                    prodotto = new Prodotto();
+                    prodotto.setId(id);
+                    prodotto.setNome(nome);
+                    prodotto.setDisponibilita(disponibilita);
+                    prodotto.setCategoria(categoria);
+                    prodotto.setDescrizione(descrizione);
+                    prodotto.setPrezzo_Cent(prezzo_Cent);
+                    try {
+                        PrDAO.doUpdateProdotto(prodotto);
+                    } catch(Exception e) {
+                        throw new MyServletException("Prodotto non trovato.");
+                    }
+                    request.setAttribute("messaggio", "Prodotto aggiornato con successo.");
+                    requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/notifica.jsp");
                     requestDispatcher.forward(request, response);
                 }
                 break;
@@ -64,12 +108,15 @@ public class GestioneProdottoController extends HttpServlet {
             case "/prodotto-aggiungi":
                 ut = (UtenteRegistrato) request.getSession().getAttribute("utente");
                 if(ut != null && ut.getTipo() == 1) {
-                    String nome = request.getParameter("nome");
-                    String descrizione = request.getParameter("descrizione");
-                    String categoria = request.getParameter("categoria");
-                    System.out.println(request.getParameter("descrizione"));
-                    long prezzo_Cent = Long.parseLong(request.getParameter("prezzo_Cent"));
-                    int disponibilita = Integer.parseInt(request.getParameter("disponibilita"));
+                    try {
+                        nome = request.getParameter("nome");
+                        descrizione = request.getParameter("descrizione");
+                        categoria = request.getParameter("categoria");
+                        prezzo_Cent = Long.parseLong(request.getParameter("prezzo_Cent"));
+                        disponibilita = Integer.parseInt(request.getParameter("disponibilita"));
+                    }   catch (Exception e) {
+                        throw new MyServletException("Dati non corretti.");
+                    }
                     prodotto = new Prodotto();
                     prodotto.setNome(nome);
                     prodotto.setDisponibilita(disponibilita);
@@ -77,7 +124,7 @@ public class GestioneProdottoController extends HttpServlet {
                     prodotto.setDescrizione(descrizione);
                     prodotto.setPrezzo_Cent(prezzo_Cent);
                     PrDAO.doSaveProdotto(prodotto);
-                    request.setAttribute("messaggio", "Prodotto aggiornato con successo.");
+                    request.setAttribute("messaggio", "Prodotto aggiunto con successo.");
                     requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/notifica.jsp");
                     requestDispatcher.forward(request, response);
                 } else {
@@ -87,7 +134,7 @@ public class GestioneProdottoController extends HttpServlet {
 
             case "/prodotto-aggiungi-form":
                 ut = (UtenteRegistrato) request.getSession().getAttribute("utente");
-                if(ut.getTipo() == 1) {
+                if(ut != null && ut.getTipo() == 1) {
                     requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/aggiungiProdotto.jsp");
                     requestDispatcher.forward(request, response);
                 } else {
@@ -117,26 +164,7 @@ public class GestioneProdottoController extends HttpServlet {
                 requestDispatcher.forward(request, response);
                 break;
 
-            case "/prodotto-modifica":
-                id = Integer.parseInt(request.getParameter("id"));
-                String nome = request.getParameter("nome");
-                String descrizione = request.getParameter("descrizione");
-                String categoria = request.getParameter("categoria");
-                System.out.println(request.getParameter("descrizione"));
-                long prezzo_Cent = Long.parseLong(request.getParameter("prezzo_Cent"));
-                int disponibilita = Integer.parseInt(request.getParameter("disponibilita"));
-                prodotto = new Prodotto();
-                prodotto.setId(id);
-                prodotto.setNome(nome);
-                prodotto.setDisponibilita(disponibilita);
-                prodotto.setCategoria(categoria);
-                prodotto.setDescrizione(descrizione);
-                prodotto.setPrezzo_Cent(prezzo_Cent);
-                PrDAO.doUpdateProdotto(prodotto);
-                request.setAttribute("messaggio", "Prodotto aggiornato con successo.");
-                requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/notifica.jsp");
-                requestDispatcher.forward(request, response);
-                break;
+
         }
 
 
