44c7dc29984893bac22efa6ae747f10e99c5adf7
Giuseppe2699 <32338720+peppe0@users.noreply.github.com>
2022-01-23 12:50:14 +0100

    modificato AutenticazioneServiceImp

--- src/main/java/PartnerShop/autenticazione/service/AutenticazioneServiceImp.java
+++ src/main/java/PartnerShop/autenticazione/service/AutenticazioneServiceImp.java
@@ -26,7 +26,7 @@ public class AutenticazioneServiceImp implements AutenticazioneService{
         }catch(IOException e ){
             e.printStackTrace();
         }
-        if(ut != null && ut.getTipo() == 0){
+        if(ut!=null && ut.getTipo()==0) {
             CarrelloDAO car = new CarrelloDAO();
             int id_carrello = car.doRetrieveIdCarrelloByEmailCliente(ut.getEmail());
             ut.setId_Carrello(id_carrello);
