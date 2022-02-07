9682c3b5a9c7334d528399eb6f0158db57660ffd
Giuseppe2699 <32338720+peppe0@users.noreply.github.com>
2022-02-07 13:43:15 +0100

    aggiunto AutenticazioneIntegrazione

--- src/test/java/IntegrationTesting/ControllerEscluso/AutenticazioneTest.java
+++ src/test/java/IntegrationTesting/ControllerEscluso/AutenticazioneTest.java
@@ -1,17 +1,13 @@
-package IntegrationTesting;
+package IntegrationTesting.ControllerEscluso;
 
 import PartnerShop.autenticazione.controller.AutenticazioneController;
+import PartnerShop.autenticazione.service.AutenticazioneService;
 import PartnerShop.autenticazione.service.AutenticazioneServiceImp;
-import PartnerShop.gestioneProdotto.controller.GestioneProdottoController;
-import PartnerShop.gestioneProdotto.service.GestioneProdottoService;
 import PartnerShop.model.dao.UtenteRegistratoDAO;
-import PartnerShop.model.entity.Prodotto;
 import PartnerShop.model.entity.UtenteRegistrato;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockHttpSession;
@@ -19,9 +15,7 @@ import org.springframework.mock.web.MockHttpSession;
 import javax.servlet.ServletException;
 import java.io.IOException;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 public class AutenticazioneTest {
@@ -30,7 +24,7 @@ public class AutenticazioneTest {
     MockHttpSession session;
     UtenteRegistrato utenteMock;
     UtenteRegistratoDAO utDBMock;
-    AutenticazioneController autenticazioneController;
+    AutenticazioneService autService;
 
 
     @Before
@@ -41,18 +35,26 @@ public class AutenticazioneTest {
         session = new MockHttpSession();
         utenteMock=mock(UtenteRegistrato.class);
         utDBMock = mock(UtenteRegistratoDAO.class);
-        autenticazioneController = new AutenticazioneController();
+        autService = new AutenticazioneServiceImp();
     }
 
     @Test
-    public void AutenticazioneUtenteTest() throws IOException, ServletException {
-        request.setParameter("usernameLogin","ancona1");
-        request.setParameter("passwordLogin","asd");
-        autenticazioneController.doPost(request,response);
-        UtenteRegistrato ut = (UtenteRegistrato)request.getSession().getAttribute("utente");
-        when(utDBMock.doRetrieveByUsernamePass(anyString(),anyString())).thenReturn(utenteMock);
-        when(utenteMock.getTipo()).thenReturn(2);
+    public void loginUtenteUsernameFormatoErratoTest() throws IOException, ServletException {
+        String username = "ancona1.";
+        String password = "asd";
+        UtenteRegistrato ut =autService.login(username,password);
+        System.out.println(ut);
+        Assert.assertEquals(null,ut);
+    }
+
+    @Test
+    public void loginOkTest() throws IOException, ServletException {
+        String username = "ancona1";
+        String password = "asd";
+        UtenteRegistrato ut =autService.login(username,password);
         System.out.println(ut);
         Assert.assertNotEquals(null,ut);
     }
+
+
 }
