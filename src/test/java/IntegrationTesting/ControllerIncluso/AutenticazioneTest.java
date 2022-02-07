0df4357848ebbd1a10df25deac2308c72043349f
Giuseppe2699 <32338720+peppe0@users.noreply.github.com>
2022-02-07 13:43:15 +0100

    aggiunto AutenticazioneIntegrazione

---
+++ src/test/java/IntegrationTesting/ControllerIncluso/AutenticazioneTest.java
@@ -0,0 +1,68 @@
+package IntegrationTesting.ControllerIncluso;
+
+import PartnerShop.autenticazione.controller.AutenticazioneController;
+import PartnerShop.autenticazione.service.AutenticazioneServiceImp;
+import PartnerShop.gestioneProdotto.controller.GestioneProdottoController;
+import PartnerShop.gestioneProdotto.service.GestioneProdottoService;
+import PartnerShop.model.dao.UtenteRegistratoDAO;
+import PartnerShop.model.entity.Prodotto;
+import PartnerShop.model.entity.UtenteRegistrato;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class AutenticazioneTest {
+    MockHttpServletRequest request;
+    MockHttpServletResponse response;
+    MockHttpSession session;
+    UtenteRegistrato utenteMock;
+    UtenteRegistratoDAO utDBMock;
+    AutenticazioneController autenticazioneController;
+
+
+    @Before
+    public void setUp(){
+
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        session = new MockHttpSession();
+        utenteMock=mock(UtenteRegistrato.class);
+        utDBMock = mock(UtenteRegistratoDAO.class);
+        autenticazioneController = new AutenticazioneController();
+    }
+
+    @Test
+    public void loginUtenteUsernameFormatoErratoTest() throws IOException, ServletException {
+        request.setParameter("usernameLogin","ancona1.");
+        request.setParameter("passwordLogin","asd");
+        autenticazioneController.doPost(request,response);
+        UtenteRegistrato ut = (UtenteRegistrato)request.getSession().getAttribute("utente");
+        Assert.assertEquals(null,ut);
+    }
+
+
+    @Test
+    public void loginOkTest() throws IOException, ServletException {
+        request.setParameter("usernameLogin","ancona1");
+        request.setParameter("passwordLogin","asd");
+        autenticazioneController.doPost(request,response);
+        UtenteRegistrato ut = (UtenteRegistrato)request.getSession().getAttribute("utente");
+        Assert.assertNotEquals(null,ut);
+    }
+
+
+
+}
