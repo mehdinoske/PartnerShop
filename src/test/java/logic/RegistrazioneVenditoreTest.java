f1f42a36d51d17ea2591a7025ff09c7059eb53a8
Giuseppe2699 <32338720+peppe0@users.noreply.github.com>
2022-02-07 12:32:53 +0100

    fix autenticazioneIntegration

--- src/test/java/logic/RegistrazioneVenditoreTest.java
+++ src/test/java/logic/RegistrazioneVenditoreTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 @Ignore
 public class RegistrazioneVenditoreTest {
 
