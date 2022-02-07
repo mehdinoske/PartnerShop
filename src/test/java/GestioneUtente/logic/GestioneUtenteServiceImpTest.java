11e0f2538d3c904f6a523ee800e0d76a646e1365
gitmarcuzz <m.ancona2@studenti.unisa.it>
2022-02-07 12:30:19 +0100

    unit test gestioneutente

--- src/test/java/GestioneUtente/logic/GestioneUtenteServiceImpTest.java
+++ src/test/java/GestioneUtente/logic/GestioneUtenteServiceImpTest.java
@@ -1,10 +1,23 @@
 package GestioneUtente.logic;
 
+import PartnerShop.GestioneUtenti.model.GestioneUtenteServiceImp;
+import PartnerShop.model.dao.UtenteRegistratoDAO;
+import PartnerShop.model.entity.UtenteRegistrato;
+import PartnerShop.registrazione.service.RegistrazioneServiceImp;
 import org.junit.Before;
+import org.mockito.Mockito;
 
 public class GestioneUtenteServiceImpTest {
+
+    GestioneUtenteServiceImp utService;
+    UtenteRegistrato utMock;
+    UtenteRegistratoDAO utDBMock;
+
     @Before
     public void setup(){
+        utMock = Mockito.mock(UtenteRegistrato.class);
+        utDBMock = Mockito.mock(UtenteRegistratoDAO.class);
+        this.utService = new GestioneUtenteServiceImp();
 
     }
 }
